708a0b137e586e337743836320618e70931fcb92
andreyyy32 <andreyyy32@mail.ru>
2022-12-11 16:55:01 +0300

    lazy load

--- site_blog/settings.py
+++ site_blog/settings.py
@@ -64,17 +64,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'u1472757_default',
-        # 'USER': 'u1472757_default',
-        # 'PASSWORD': 's87jZz4y9aJPm1hT',
-        # 'HOST': 'localhost',
-        # 'OPTIONS': {
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        #     'charset': 'utf8mb4',
-        },
     }
-
+}
 
 
 # Password validation
