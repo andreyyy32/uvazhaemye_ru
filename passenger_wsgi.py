554f0bcc61416aa886f2fbc3845850c236320c2b
Andrey <55717431+andreyyy32@users.noreply.github.com>
2023-06-21 00:03:14 +0300

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/var/www/u1472757/data/www/uvazhaemye.ru/site_blog')
-sys.path.insert(1, '/var/www/u1472757/data/uvazhaemyeenv/lib/python3.9/site-packages')
+sys.path.insert(0, '/var/www/***********/data/www/uvazhaemye.ru/site_blog')
+sys.path.insert(1, '/var/www/***********/data/uvazhaemyeenv/lib/python3.9/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'site_blog.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
